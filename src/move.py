587dfdbb443366a98a9bd3a2bef9c707fcc66e61
Rahul Gopathi <75736709+RahulGopathi@users.noreply.github.com>
2021-07-08 23:48:01 +0530

    Update move.py

--- src/move.py
+++ src/move.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import time
-
 import numpy
 import rospy
 import time
@@ -11,11 +10,17 @@ from numpy import array, inf
 
 class TurtleBot:
     def __init__(self):
+        # This constructor will initiate all the readings required navigation
         self.vel = Twist()
+
+        # flag for knowing if the bot is out of maze
         self.out_of_maze = False
         self.turtlebot_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
         self.turtlebot_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
         self.ranges = []
+
+        # initializing Regions with the inf readings 
+        # here inf = 10 
         self.regions = {'bright': 10,
                         'right': 10,
                         'fright': 10,
@@ -29,10 +34,11 @@ class TurtleBot:
                              self.regions['front'],
                              self.regions['right'],
                              self.regions['bright']]
-        self.rate = rospy.Rate(5)  # 1hz
+        self.rate = rospy.Rate(5)  # 5hz
 
     def scan_callback(self, msg):
-        # self.ranges = list(msg.ranges)
+
+        # callback function after recieving the readings from /LasersScan topic
         self.ranges = array(msg.ranges)
         self.ranges[self.ranges == inf] = 10
         self.regions = {
@@ -46,11 +52,13 @@ class TurtleBot:
         }
 
     def algorithm(self):
+
         while not self.out_of_maze:
-            # print "left:", self.left
-            # # print "front_avg:", self.front_average
-            # # print "front", self.front
-            print "regions:", self.regions
+            
+            # The minimum desired value from the wall is kept as 0.35
+            # The values above 0.9 will become 2
+            # The Values between 0.35 and 0.9 will become 1
+            # The values less than 0.35 will become 0
             for key in self.regions.keys():
                 if self.regions[key] >= 0.9:
                     self.regions[key] = 2
@@ -64,29 +72,46 @@ class TurtleBot:
                                  self.regions['front'],
                                  self.regions['right'],
                                  self.regions['bright']]
-            print "surroundings:", self.surroundings
+            
+            # moving right when the right region is inf and backright region is not inf
             if self.surroundings[4:] == [2, 1]:
                 self.vel.angular.z = -1.5
                 self.vel.linear.x = 0.2
+            
+            # moving right when the right region is inf and backright region is not inf but very near to wall
             elif self.surroundings[4:] == [2, 0]:
                 self.vel.angular.z = -1.5
                 self.vel.linear.x = 0.2
+            
+            # If there is wall in front
             elif self.surroundings[3] == 1:
+
+                # rotating left if left values are greater than right values
                 if self.surroundings[0] >= self.surroundings[1]:
                     self.vel.linear.x = 0.1
                     self.vel.angular.z = 0.5
+                
+                # rotating right if right values are greater than left values
                 else:
                     self.vel.angular.z = -0.5
                     self.vel.linear.x = 0.1
+            
+            # tilting somewhat right if it is too near to a wall on left side
             elif self.surroundings[0] == 0:
                 self.vel.linear.x = 0.09
                 self.vel.angular.z = -0.2
+            
+            # tilting somewhat left if it is too near to a wall on right side
             elif self.surroundings[1] == 0:
                 self.vel.linear.x = 0.09
                 self.vel.angular.z = 0.2
+            
+            # checking if it is out of maze
             elif self.surroundings[:4] == [2, 2, 2, 2, 2]:
                 self.vel.linear.x = 0
                 self.vel.angular.z = 0
+            
+            # moving straight without any angular velocity if none of the condition satisfied
             else:
                 self.vel.linear.x = 0.3
                 self.vel.angular.z = 0
@@ -96,7 +121,9 @@ class TurtleBot:
 
 if __name__ == '__main__':
     rospy.init_node('rosbot_test', anonymous=True)
+
     turtlebot = TurtleBot()
+
     try:
         turtlebot.algorithm()
 
